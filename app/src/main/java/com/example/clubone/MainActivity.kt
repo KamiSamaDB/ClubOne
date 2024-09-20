bff91245e15431f6e69f2f0de3acc1a580851fc3
KamiSamaDB <ashwinder1225@gmail.com>
2024-09-20 19:42:26 +0530

    Commit #1

--- app/src/main/java/com/example/clubone/MainActivity.kt
+++ app/src/main/java/com/example/clubone/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.clubone
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+   lateinit var usernameInput: EditText
+   lateinit var passwordInput: EditText
+   lateinit var loginButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginButton = findViewById(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i("Test Credentials", "Username: $username, Password: $password")
+        }
+
     }
 }
